772c9ed2cb4caed19116d84b6f2db461e63ca81a
Paul Marshall <ptm@esarks.com>
2025-06-09 11:54:17 -0400

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,5 @@
 export const fetchVerses = async (book, chapter) => {
-
-  const response = await fetch(`https://my-bible-frontend-327765964554.us-central1.run.app/api/bible?book=${book}&chapter=${chapter}`);
+  const response = await fetch(`https://my-bible-backend-<numbers>.a.run.app/api/bible?book=${book}&chapter=${chapter}`);
 
   if (!response.ok) {
     throw new Error('Failed to fetch verses');
